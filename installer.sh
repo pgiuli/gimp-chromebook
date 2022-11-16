f2675c8dff9e89891c0d1624daf2f9316cf611a4
Pau Giuli <76668405+spxrked@users.noreply.github.com>
2022-11-16 17:30:47 +0100

    Updated main file

--- installer.sh
+++ installer.sh
@@ -2,10 +2,16 @@
 
 clear
 
-echo "Instal·lador de GIMP per a Chromebook v0.1.0 - Pau Giuli 2022"
+echo "Instal·lador de GIMP per a Chromebook v1.2.1 - Pau Giuli 2022"
 echo "Aquest procés pot trigar uns minuts."
 read -p "Prem [Enter] enter per a iniciar la instal·lació."
 
 sudo apt-get update
 sudo apt-get install gimp -y
 
+
+clear
+
+echo "Procés finalitzat!"
+echo "Ja hauries de poder veure la icona del programa al calaix d'aplicacions."
+echo "Ara pots tancar aquesta finestra. Gràcies!"
