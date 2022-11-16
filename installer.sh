7bd5ea59ca8ff7733962877d0034e8ca803c638a
Pau Giuli <76668405+pgiuli@users.noreply.github.com>
2022-11-16 20:41:09 +0100

    Removed extra word

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@ clear
 
 echo "Instal·lador de GIMP per a Chromebook v1.2.1 - Pau Giuli 2022"
 echo "Aquest procés pot trigar uns minuts."
-read -p "Prem [Enter] enter per a iniciar la instal·lació."
+read -p "Prem [Enter] per a iniciar la instal·lació."
 
 sudo apt-get update
 sudo apt-get install gimp -y
